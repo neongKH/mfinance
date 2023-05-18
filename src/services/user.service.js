42885536cf25a48aeb28e288fefe276077879224
vadimyff <solpo789@gmail.com>
2023-05-18 20:59:55 +0300

    save

--- src/services/user.service.js
+++ src/services/user.service.js
@@ -1,14 +1,23 @@
 import axios from "axios";
 
-const API_URL = "https://mfinancial.herokuapp.com/apiauth/jwt/";
-
+const API_URL = "https://mfinancial.herokuapp.com/";
+const API = "https://mfinancial.herokuapp.com/transactions/1";
 class AuthService {
 	async login(user) {
-		const response = await axios.post(API_URL + "create", {
+		const response = await axios.post(API_URL + "apiauth/jwt/create", {
 			username: user.username,
 			password: user.password,
 		});
 		localStorage.setItem("user", JSON.stringify(response.data));
+		return response.data;
+	}
+
+	async transactions(transactions) {
+		const response = await axios.get(API, {
+			user: transactions.user,
+			category: transactions.category
+		});
+		localStorage.setItem("transaction", JSON.stringify(response.data));
 		return response.data;
 	}
 
@@ -17,7 +26,7 @@ class AuthService {
 	}
 
 	register(user) {
-		return axios.post(API_URL + "register", { // Update the endpoint URL to the registration endpoint
+		return axios.post(API_URL + "user", { // Update the endpoint URL to the registration endpoint
 		  username: user.username,
 		  email: user.email,
 		  password: user.password,
